e9be5f1cd39d27bee6ab03939a0109301418654e
Nikola Borisic <borisic.nikola@yahoo.com>
2019-07-18 15:28:42 +0200

    add redirects

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+FILENAME=_redirects
+
 if [ -d "./build" ]
 then
-    rm -rf build && parcel build index.html --out-dir build
+    rm -rf build && parcel build public/index.html --out-dir build && \
+    touch ./build/_redirects && \
+    cat << EOF > ./build/_redirects
+    /*    /index.html   200
+EOF
 else
-    parcel build index.html --out-dir build
+    parcel build public/index.html --out-dir build && \
+    touch ./build/_redirects && \
+    cat << EOF > touch ./build/_redirects
+    /*    /index.html   200
+EOF
 fi
